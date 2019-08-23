471a7b20e18260f4debf0caa18045203b9dfdab2
Gokul <32118741+Gokul2104@users.noreply.github.com>
2019-08-23 19:20:49 +0530

    insta added

---
+++ insta/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="jquery.fancybox-1.3.4.css" type="text/css">
+	<script type='text/javascript' src='jquery.min.js'></script>
+	<script type='text/javascript' src='jquery.fancybox-1.3.4.pack.js'></script>
+	<script type="text/javascript">
+		$(function() {
+			$("a.group").fancybox({
+				'nextEffect'	:	'fade',
+				'prevEffect'	:	'fade',
+				'overlayOpacity' :  0.8,
+				'overlayColor' : '#000000',
+				'arrows' : false,
+			});
+		});
+	</script>
+
+	<?php
+		// Supply a user id and an access token
+		$userid = "self";
+		$accessToken = "9988719258.e000e4a.d4e91c8994574ad29921ade07333e0e4";
+		// Gets our data
+		function fetchData($url){
+		     $ch = curl_init();
+		     curl_setopt($ch, CURLOPT_URL, $url);
+		     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		     curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+		     $result = curl_exec($ch);
+		     curl_close($ch);
+		     return $result;
+		}
+
+		// Pulls and parses data.
+		$result = fetchData("https://api.instagram.com/v1/users/{$userid}/media/recent/?access_token={$accessToken}");
+		#https://api.instagram.com/v1/users/self/?access_token=9988719258.e000e4a.d4e91c8994574ad29921ade07333e0e4
+		#echo $result;
+		$result = json_decode($result);
+	?>
+
+
+	<?php foreach ($result->data as $post): ?>
+		<!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
+		<a class="group" rel="group1" href="<?= $post->images->standard_resolution->url ?>"><img src="<?= $post->images->thumbnail->url ?>"></a>
+	<?php endforeach ?>
+</html>
